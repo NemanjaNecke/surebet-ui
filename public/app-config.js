3feebc7f3dd995a086e45ada3361423a9ac224b7
Nemanja <n.filkic@gmail.com>
2026-08-30 23:50:44 +0200

    Point production UI at KvotaRadar API

--- public/app-config.js
+++ public/app-config.js
@@ -1,5 +1,5 @@
 window.__SUREEDGE_CONFIG__ = {
-  apiBaseUrl: 'https://surebet-api.samnangsattva.workers.dev/api/v1',
+  apiBaseUrl: 'https://api.kvotaradar.online/api/v1',
   auth: {
     domain: 'dev-sj137jrezlkzv1dx.us.auth0.com',
     clientId: '1r92ZDTc5JOy3ClgLOmmWVcXorMex5z3',
